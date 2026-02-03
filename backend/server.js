73b96886f71cd4b6867505c3c000f777ed596aa9
rumman2004 <sahnazy99@gmail.com>
2026-02-04 04:21:00 +0530

    DOMAIN CHECK

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL && process.env.CLIENT_URL_ALT || 'http://localhost:5173',
     credentials: true
 }));
 
